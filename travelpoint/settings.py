67f5b9df219b9e6e2a08e2ef1618c222eabfdd9b
JATeR912 <jater.code@gmail.com>
2025-11-22 14:09:26 -0300

    Configuracion entorno

--- travelpoint/settings.py
+++ travelpoint/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'travelpoint.urls'
@@ -133,6 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Configuración de archivos multimedia
 MEDIA_URL = '/media/'
